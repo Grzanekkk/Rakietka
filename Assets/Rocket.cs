e305e791d15f301d3756a36d3fff68996ec35d2d
Jan Puto <tofukles@gmail.com>
2020-08-21 15:41:02 +0200

    Rotation and Audio

---
+++ Assets/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed = 18;
+    public float turnSpeed = 15;
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * speed);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward, turnSpeed * Time.deltaTime);
+        }
+    }
+}

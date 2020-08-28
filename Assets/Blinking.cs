c137f3b3c399f442277e3d47736f15233133dac8
Jan Puto <tofukles@gmail.com>
2020-08-28 12:50:39 +0200

    Level 1 Finished

---
+++ Assets/Blinking.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Blinking : MonoBehaviour
+{
+    public bool blink = true;
+    public float delay = 1;
+    bool isBlinking;
+    Light light;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+    void Update()
+    {
+        if (blink)
+        {
+            if (!isBlinking)
+            {
+                StartCoroutine(Blink());
+            }
+        }
+        else
+        {
+            StopCoroutine(Blink());
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        light.enabled = false;
+        yield return new WaitForSeconds(delay);
+        light.enabled = true;
+        yield return new WaitForSeconds(delay);
+    }
+}

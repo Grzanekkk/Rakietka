79aa69259ead0d1d0b1c0ab624e7ef32ce8875cc
Jan Puto <tofukles@gmail.com>
2020-09-07 21:00:42 +0200

    Sound and Particles

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -8,6 +10,23 @@ public class Rocket : MonoBehaviour
     public float turnSpeed = 15f;
     Rigidbody rb;
     AudioSource audioSource;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip win;
+    [SerializeField] AudioClip death;
+    
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem winParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    enum State
+    {
+        Alive,
+        Dead,
+        Transcending
+    }
+
+    State state = State.Alive;
 
     void Start()
     {
@@ -17,9 +36,13 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-        Thrust();
+        if (state != State.Dead)
+        {
+            Thrust();
 
-        Rotate();
+            Rotate();
+        }
+
     }
 
     private void Thrust()
@@ -29,26 +52,15 @@ public class Rocket : MonoBehaviour
             rb.AddRelativeForce(Vector3.up * speed);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
             }
+
+            mainEngineParticles.Play();
         }
         else
         {
             audioSource.Stop();
-        }
-    }
-
-    void OnCollisionEnter(Collision enterCollision)
-    {
-        print("Collision");
-
-        if (enterCollision.gameObject.CompareTag("Finish"))
-        {
-            print("You Win!");
-        }
-        else if(enterCollision.gameObject.CompareTag("Obstacle"))
-        {
-            print("You lost your fUeL LmAO niCe");
+            mainEngineParticles.Pause();
         }
     }
 
@@ -69,4 +81,55 @@ public class Rocket : MonoBehaviour
 
         rb.freezeRotation = false;
     }
+
+    void OnCollisionEnter(Collision enterCollision)
+    {
+        if (state != State.Alive)
+            return;
+        
+        string collisionTag = enterCollision.gameObject.tag;
+
+        if (collisionTag == "Finish")
+        {
+            LevelFinished();
+        }
+        else if(collisionTag == "Obstacle")
+        {
+            print("You lost your fUeL LmAO niCe");
+        }
+        else if(collisionTag == "Deadly")
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        state = State.Dead;
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        print("You died XDDD");
+        Invoke("ReloadLevel", 1f);
+    }
+
+    void LevelFinished()
+    {
+        state = State.Transcending;
+        audioSource.PlayOneShot(win);
+        winParticles.Play();
+        print("You Win!");
+        Invoke("LoadNextScene", 1f);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
 }
